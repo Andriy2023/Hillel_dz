380a64fb17530eae7fc7b2cb88b73e5e4fe85835
Andriy2023 <abv1-91@ukr.net>
2023-07-16 03:14:42 +0300

    dz4 number change

--- dz4.py
+++ dz4.py
@@ -1,4 +1,13 @@
-# we have four values w,x,y,z
-# write if-elif-else statement that will search minimum value and print smth aka "'y' is minimum value'
-# advice use python debugger and different values to check your algorithm
+"""Searching the minimum value."""
 w, x, y, z = 100, 200, 40, 300
+
+# Comparing variables to find the minimum value
+if w <= x and w <= y and w <= z:
+    print("'w' is the minimum value")
+elif x <= y and x <= z:
+    print("'x' is the minimum value")
+elif y <= z:
+    print("'y' is the minimum value")
+else:
+    print("'z' is the minimum value")
+    
