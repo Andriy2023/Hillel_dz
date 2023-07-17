05d6a7a431b73dfe6b780ad522483c96c39d3ad2
Andriy2023 <abv1-91@ukr.net>
2023-07-17 22:42:24 +0300

    changes dz5

--- dz5.py
+++ dz5.py
@@ -1,7 +1,6 @@
-# Exists some car data with color, year, engine_volume, car type , price
-# We have search_criteria as tuple of ( year>= , engine_volume >= , price<=)
-# write code that will help us to get cars that satisfy search_criteria.
-# Cars should be sorted by price ascending. We should print up to five first found elements
+"""Sorting cars by price."""
+
+# Cars in the dictionary
 car_data = {
     'Mercedes': ('silver', 2019, 1.8, 'sedan', 50000),
     'Audi': ('black', 2020, 2.0, 'sedan', 55000),
@@ -41,6 +40,26 @@ car_data = {
     'Chevy': ('red', 2017, 2.4, 'pickup', 35000),
     'Dodge Ram': ('white', 2020, 3.6, 'pickup', 45000),
     'Ford F-Series': ('gray', 2021, 3.5, 'pickup', 50000),
-    'Nissan Titan': ('silver', 2018, 5.6, 'pickup', 35000)
+    'Nissan Titan': ('silver', 2018, 5.6, 'pickup', 35000),
 }
+# Sort criteria
 search_criteria = (2017, 1.6, 36000)
+
+# function with two arguments from a dictionary and a criterion
+def filter_and_sort_cars(car_data, search_criteria):
+    filtered_cars = []
+    for car, data in car_data.items():
+        if data[1] >= search_criteria[0] \
+                and data[2] >= search_criteria[1] \
+                and data[4] <= search_criteria[2]:
+            filtered_cars.append((car, data[0], data[1],
+                                  data[2], data[3], data[4]))
+#Sort the resulting list by key
+    sorted_cars = sorted(filtered_cars, key=lambda x: x[5])
+# list of 5 lines
+    return sorted_cars[:5]
+# writing a function to a variable and printing
+found_cars = filter_and_sort_cars(car_data, search_criteria)
+for car in found_cars:
+    print(f"Car: {car[0]}, Color: {car[1]}, Year: {car[2]}, "
+          f"Engine Volume: {car[3]}, Type: {car[4]}, Price: {car[5]}")
