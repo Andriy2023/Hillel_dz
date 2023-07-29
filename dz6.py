81ceddde0401b0ce0ff20fc5c450641fb8dfff16
Andriy2023 <abv1-91@ukr.net>
2023-07-29 13:16:06 +0300

    change dz6

--- dz6.py
+++ dz6.py
@@ -1,32 +1,32 @@
-"""Compiling a new list using  comprehension."""
-
-from itertools import zip_longest
+"""Compiling a new list using comprehension."""
 
 # lists № 1
-l1 = [1, 3, 5, 7]
-l2 = [1, 4, 5]
-
-# lists № 2
-# l1 = [1, 4, 5]
-# l2 = [1, 3, 5, 7]
-
-# lists № 3
 # l1 = [2, 4, 6, 8, 10]
 # l2 = [1, 2, 3]
 
-# Get the maximum length between the two lists
-max_length = max(len(l1), len(l2))
+# lists № 2
+l1 = [1, 3, 5, 7]
+l2 = [1, 4, 5]
 
-# Using zip_longest to pair up elements and
-# filling the missing elements digit 0
-var1 = zip_longest(l1, l2, fillvalue=0)
+# lists № 3
+# l1 = [1, 4, 5]
+# l2 = [1, 3, 5, 7]
 
-# Create a dictionary with a default value of 0
-result_dict = {i: (x, y) for i, (x, y) in enumerate(var1)}
+# Finding common indexes from both lists
+index_l1 = set(range(len(l1)))
+index_l2 = set(range(len(l2)))
+index_union = index_l1.union(index_l2)
+
+
+# Creating a dictionary of values
+result_dict = {i: (l1[i] if i < len(l1) else 0,
+                   l2[i] if i < len(l2) else 0)
+               for i in index_union}
+
 
 # Create the list_target using a list comprehension,
-# taking values from the dictionary result_dict
+# taking values from the dictionary
 list_target = [(result_dict[i][0], result_dict[i][1])
-               for i in range(max_length)]
+               for i in range(len(index_union))]
 
 print(list_target)
